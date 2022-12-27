3d20685866c1da8e4f13578b38eec2d303f00c41
andor <latandor@gmail.com>
2022-12-27 15:38:33 -0500

    api get request enpoint

--- server/server.js
+++ server/server.js
@@ -3,11 +3,17 @@ const app = express()
 const cors = require('cors')
 const pool = require('./db')
 
-app.get('/api', (req, res) => {
-  res.json({ users: ['userOne', 'userTwo', 'userThree'] })
-})
-
 //ROUTES//
+app.use(cors())
+app.use(express.json())
+app.get('/api', async (req, res) => {
+  try {
+    const { id } = req.params
+    const todo = await pool.query('SELECT * FROM public.question_one_shifts')
+  } catch (error) {
+    console.log(error.message)
+  }
+})
 
 app.listen(5000, () => {
   console.log('Server started on port 5000')
