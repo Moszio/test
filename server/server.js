84f10de82756f4eb8aca404bc3be72b6e3ff8315
andor <latandor@gmail.com>
2022-12-27 15:01:21 -0500

    database connection to postgresql

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const pool = require('./db')
 
 app.get('/api', (req, res) => {
   res.json({ users: ['userOne', 'userTwo', 'userThree'] })
 })
+
+//ROUTES//
 
 app.listen(5000, () => {
   console.log('Server started on port 5000')
